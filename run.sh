0f0a3af876b3f737378142c1d494ffda8146e196
Hyoukjun Kwon <hyoukjun@lawn-128-61-74-243.lawn.gatech.edu>
2019-02-25 14:36:50 -0500

    Fixing reference run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-./maestro --dataflow_file='data/dataflow/prs.m' \
+./maestro --dataflow_file='data/dataflow/rs.m' \
           --layer_file='data/layer/vgg16_conv2.m' \
           --noc_bw=4 \
           --num_pes=16
